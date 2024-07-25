db1eea6018e54e6a0c33f4dd7fa159437c50b15f
kenneri94 <kenneri94@gmail.com>
2024-07-25 08:40:42 -0700

    6. Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+/* Write a script that prints 3 lines by using an array of string and a loop */
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
