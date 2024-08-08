17740bcc9268cad9a2f30e84f18a56a2deca99ed
kenneri94 <kenneri94@gmail.com>
2024-08-07 17:49:00 -0700

    5. Loripsum

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,4 +1,10 @@
 #!/usr/bin/node
 const fs = require('fs');
 const request = require('request');
-request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
+request(process.argv[2], function (error, response, body) {
+  fs.writeFile(process.argv[3], body, 'utf8', function (error) {
+    if (error) {
+      console.log(error);
+    }
+  });
+});
